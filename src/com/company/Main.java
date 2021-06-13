30ae00a2180c49805989b5051452976adaea822e
Jahanzeb2021 <jahanilyas3@gmail.com>
2021-06-13 09:54:47 -0700

    Features of sun are added but there is an error which is still being resolver

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
          SolarSystem  FactorX=new SolarSystem();
+         FactorX.Moon=3;
          FactorX.Sun=1;
-        FactorX.Moon=3;
-        FactorX.Planates=2;
+         FactorX.Planates=2;
+
+
+       //working on sun features
+        Sun Feron =new Sun();
+        Feron.Heat="600000000000kj";
+        Feron.Radius="823587325872385723895km/2";
+        Feron.Temperature="100000000000000000000000000f";
+        
     }
 }
